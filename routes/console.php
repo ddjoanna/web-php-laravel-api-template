39e7ea834a5524fc19feacbccde9f987ed6364d2
ddjoanna <joann.chang0722@gmail.com>
2025-04-23 13:11:19 +0800

    feat: add job to execute hourly

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\Log;
+use App\Jobs\HourlyTaskJob;
+
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::call(function () {
+    Log::info('✅ Laravel scheduler triggered at: ' . now());
+})->everyMinute();
+
+Schedule::job(new HourlyTaskJob)->hourly();
